4b79c67b017f2fb6db601caac1d34e2fb87adcf1
In, Sangjin <insangjin6439@naver.com>
2023-09-25 12:21:33 +0900

    main

---
+++ Bronze3/2525 오븐시계.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+
+        a+=c/60;
+        b+=c%60;
+
+        if(b>=60){
+            a+=b/60;
+        }
+
+        if (a>=24) {
+            a-=24;
+        }
+        System.out.println(a+" "+b%60);
+        }
+
+    }
+
